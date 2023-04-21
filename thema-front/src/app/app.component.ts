6c711ff83fd3bbdb73df071672715c859b1b971d
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-04-22 00:32:36 +0200

    [front] bind back to front

--- thema-front/src/app/app.component.ts
+++ thema-front/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {apiUrl} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class AppComponent {
   title = 'thema-front';
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
+
+  word: string = '';
+  synonyms: string[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  onFormSubmit() {
+    // Fetch synonyms using your service, e.g. via HTTP request
+    this.http.get<string[]>(apiUrl + '/synonyms/' + this.word)
+      .subscribe(
+        (data) => {
+          this.synonyms = data; // Update the synonyms array with fetched data
+        },
+        (error) => {
+          console.error('Error fetching synonyms:', error);
+        }
+      );
+  }
+
 }
--- thema-front/src/app/app.module.ts
+++ thema-front/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressBarModule} from "@angular/material/progress-bar";
-import { DevhelpComponent } from './devhelp/devhelp.component';
+import {DevhelpComponent} from './devhelp/devhelp.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {ScrollingModule} from "@angular/cdk/scrolling";
 
 @NgModule({
   declarations: [
@@ -31,7 +34,10 @@ import { DevhelpComponent } from './devhelp/devhelp.component';
     MatMenuModule,
     MatInputModule,
     MatCardModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    FormsModule,
+    HttpClientModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

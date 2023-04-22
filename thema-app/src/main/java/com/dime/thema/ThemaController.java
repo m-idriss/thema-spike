e21af08538b1dc6281c2508de1eeebe958cf4961
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-04-23 00:16:37 +0200

    [back] update crossOrigin

--- thema-app/src/main/java/com/dime/thema/ThemaController.java
+++ thema-app/src/main/java/com/dime/thema/ThemaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @Getter
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RestController
 public class ThemaController {
 

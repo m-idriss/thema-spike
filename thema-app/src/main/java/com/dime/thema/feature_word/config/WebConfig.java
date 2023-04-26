40733d487afbcd96028ba231a38fb533b62460ee
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-04-26 11:01:05 +0200

    [back]update allowedOrigins

--- thema-app/src/main/java/com/dime/thema/feature_word/config/WebConfig.java
+++ thema-app/src/main/java/com/dime/thema/feature_word/config/WebConfig.java
@@ -2,18 +2,24 @@ package com.dime.thema.feature_word.config;
 
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(@NotNull CorsRegistry registry) {
-        WebMvcConfigurer.super.addCorsMappings(registry);
-        registry.addMapping("/**")
-                .allowedOrigins("${DOMAIN}:${ANGULAR_PORT}")
-                .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("${DOMAIN}")
+                        .allowedOrigins("${DOMAIN}:${ANGULAR_PORT}")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET");
+            }
+        };
     }
 }

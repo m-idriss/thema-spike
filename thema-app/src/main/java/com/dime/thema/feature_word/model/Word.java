191d9f3ffde2ce2ecb60fecc15a80538a4117d3e
Idriss MOHAMADY <pro.mohamady@gmail.com>
2023-05-04 11:22:12 +0200

    [dim-persistence] - start persistence

---
+++ thema-app/src/main/java/com/dime/thema/feature_word/model/Word.java
@@ -0,0 +1,29 @@
+package com.dime.thema.feature_word.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "word")
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+}

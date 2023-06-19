0fc55bc4d941170daec6759d63497b95f111c52a
Idriss Mohamady <i.mohamady@oodrive.com>
2023-06-19 21:20:14 +0200

    [dim1] - change some config

--- thema-app/src/main/java/com/dime/thema/config/WebConfig.java
+++ thema-app/src/main/java/com/dime/thema/config/WebConfig.java
@@ -1,24 +1,20 @@
 package com.dime.thema.config;
 
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NotNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*")
-                        .allowedMethods("GET");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET");
     }
+
+
 }
+
